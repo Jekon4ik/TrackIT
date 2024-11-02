3637c7c3b8dce654a57722ad422c37dcdf444bdb
Jekon4ik <ghfswhrh@gmail.com>
2024-11-02 16:15:43 +0100

    swagger implemented

--- TrackIT.Api/Program.cs
+++ TrackIT.Api/Program.cs
@@ -1,12 +1,19 @@
-using TrackIT.Api.Dtos;
-using TrackIT.Api.Data;
 using TrackIT.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
+
+
 app.MapCategoriesEndpoints();
+
 app.MapTransactionsEndpoints();
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Run();
