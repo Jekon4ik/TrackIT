a4dd311ece0ae09459dc28df8a660ed5e591c2ee
Jekon4ik <ghfswhrh@gmail.com>
2024-12-25 10:53:48 +0100

    lab1 commit

--- TrackIT.Api/obj/Debug/net8.0/TrackIT.Api.AssemblyInfo.cs
+++ TrackIT.Api/obj/Debug/net8.0/TrackIT.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackIT.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3637c7c3b8dce654a57722ad422c37dcdf444bdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4933fcdfc6d2b83f07012a2bf2f49c5edae4d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackIT.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackIT.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
